0d2b2c33aafa291011fd1c779eb65d1000d24e87
Maxim Eryomenko <moeryomenko@gmail.com>
2022-12-22 21:54:22 +0300

    solution for 70. Climbing Stairs
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

---
+++ solutions/climbing_stairs.cpp
@@ -0,0 +1,42 @@
+#include <boost/ut.hpp>
+
+#include <cmath>
+#include <float.h>
+
+constexpr auto root(double n) -> double {
+  double x = n;
+  double y = 1;
+  double e = DBL_MIN;
+  while (x - y > e) {
+    x = (x + y) / 2;
+    y = n / x;
+  }
+  return x;
+}
+
+constexpr auto root_5 = root(5);
+constexpr auto gold_ratio = (1.f + root_5) / 2;
+constexpr auto revers_gold_ratio = (1.f - root_5) / 2;
+
+auto climb_stairs(int n) -> int {
+  if (n < 0)
+    return 0;
+  if (n == 0)
+    return 1;
+  return std::round(
+      (std::pow(gold_ratio, n + 1) - std::pow(revers_gold_ratio, n + 1)) /
+      root_5);
+}
+
+auto main() -> int {
+  using namespace boost::ut;
+
+  "cases"_test = [] {
+    expect(climb_stairs(2) == 2_i);
+    expect(climb_stairs(3) == 3_i);
+    expect(climb_stairs(4) == 5_i);
+    expect(climb_stairs(35) == 14930352_i);
+  };
+
+  return 0;
+}
