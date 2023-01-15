cba5e12fac715a368b80ee2c059549685e06884f
Maxim Eryomenko <moeryomenko@gmail.com>
2023-01-15 13:02:16 +0300

    solution: 23. Merge k Sorted Lists
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- solutions/merge_sorted_lists.cpp
+++ solutions/merge_sorted_lists.cpp
@@ -1,6 +1,7 @@
 #include <boost/ut.hpp>
 
 #include <cstdlib>
+#include <numeric>
 #include <utility>
 #include <vector>
 
@@ -45,16 +46,33 @@ auto merge_sorted_lists(ListNode *list1, ListNode *list2) -> ListNode * {
   return list1;
 }
 
+auto merge_k_sorted_lits(std::vector<ListNode *> lists) -> ListNode * {
+  if (lists.empty())
+    return nullptr;
+  ListNode *res = lists.front();
+  return std::accumulate(lists.cbegin() + 1, lists.cend(), res,
+                         [](ListNode *list, ListNode *nextList) {
+                           return merge_sorted_lists(list, nextList);
+                         });
+}
+
 auto main() -> int {
   using namespace boost::ut;
 
-  "cases"_test = [] {
+  "merge_two_sorted_lists"_test = [] {
     expect(vector_from_list(merge_sorted_lists(list_from_vector({1, 2, 4}),
                                                list_from_vector({1, 3, 4}))) ==
            std::vector{1, 1, 2, 3, 4, 4});
     expect(vector_from_list(merge_sorted_lists(list_from_vector({1}),
                                                list_from_vector({2}))) ==
            std::vector{1, 2});
+  };
+
+  "merge_k_sorted_lits"_test = [] {
+    expect(vector_from_list(merge_k_sorted_lits({list_from_vector({1, 4, 5}),
+                                                 list_from_vector({1, 3, 4}),
+                                                 list_from_vector({2, 6})})) ==
+           std::vector{1, 1, 2, 3, 4, 4, 5, 6});
   };
 
   return EXIT_SUCCESS;
