5f42227beea685078c1b9863980de9b01166ae7b
Maxim Eryomenko <moeryomenko@gmail.com>
2023-01-16 22:46:31 +0300

    solution: 918. Maximum Sum Circular Subarray
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

---
+++ solutions/max_sum_circle_subarray.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <boost/ut.hpp>
+
+#include <cstdlib>
+#include <numeric>
+#include <vector>
+
+auto find_subarray_sum_circle(const std::vector<int> &nums) -> int {
+  if (nums.size() == 1)
+    return nums.back();
+
+  auto sum = std::accumulate(nums.cbegin(), nums.cend(), 0);
+
+  auto min = nums.front();
+  auto max = std::accumulate(nums.cbegin() + 1, nums.cend(), nums.front(),
+                             [&min, curr_max = nums.front(),
+                              curr_min = nums.front()](int max, int n) mutable {
+                               curr_max = std::max(curr_max + n, n);
+                               max = std::max(max, curr_max);
+
+                               curr_min = std::min(curr_min + n, n);
+                               min = std::min(min, curr_min);
+
+                               return max;
+                             });
+  if (sum == min)
+    return max;
+
+  return std::max(max, sum - min);
+}
+
+auto main() -> int {
+  using namespace boost::ut;
+
+  "cases"_test = [] {
+    expect(find_subarray_sum_circle({1, -2, 3, -2}) == 3_i);
+    expect(find_subarray_sum_circle({5, -3, 5}) == 10_i);
+    expect(find_subarray_sum_circle({-3, -2, -3}) == -2_i);
+  };
+
+  return EXIT_SUCCESS;
+}
