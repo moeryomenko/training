ff86260cedd8cf9d6c097644196326c46b69a867
Maxim Eryomenko <moeryomenko@gmail.com>
2023-01-24 22:45:31 +0300

    solution: 85. Maximal Rectangle
    - 84. Largest Rectangle in Histogram
    
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

---
+++ solutions/largest_rectangle.cpp
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include <boost/ut.hpp>
+
+#include <cstdlib>
+#include <numeric>
+#include <stack>
+#include <utility>
+#include <vector>
+
+auto largest_rectangle(const std::vector<int> &heights) -> int {
+  std::vector<std::pair<int, int>> stack;
+
+  int len = heights.size();
+
+  int max_area = 0;
+  for (int i = 0; auto h : heights) {
+    int start = i;
+    while (!stack.empty() && stack.back().second > h) {
+      auto [index, height] = stack.back();
+      stack.pop_back();
+
+      max_area = std::max(max_area, height * (i - index));
+      start = index;
+    }
+    stack.push_back({start, h});
+    ++i;
+  }
+
+  return std::accumulate(stack.cbegin(), stack.cend(), max_area,
+                         [&len](int max, auto pair) {
+                           auto [i, h] = pair;
+                           return std::max(max, h * (len - i));
+                         });
+}
+
+auto maximal_rectangle(const std::vector<std::vector<char>> &matrix) -> int {
+  std::vector<std::vector<int>> dp(matrix.size());
+  std::inclusive_scan(
+      matrix.cbegin(), matrix.cend(), dp.begin(),
+      [](std::vector<int> prev_row, std::vector<char> row) {
+        std::vector<int> res(row.size());
+        for (int i = 0; char c : row) {
+          res[i] = c == '1' ? prev_row[i] + 1 : 0;
+          ++i;
+        }
+        return res;
+      },
+      std::vector<int>(matrix.front().size(), 0));
+  return std::accumulate(dp.cbegin(), dp.cend(), 0, [](int max, auto heights) {
+    return std::max(max, largest_rectangle(heights));
+  });
+}
+
+auto main() -> int {
+  using namespace boost::ut;
+
+  "cases"_test = [] {
+    expect(largest_rectangle({2, 1, 5, 6, 2, 3}) == 10_i);
+    expect(maximal_rectangle({{'1', '0', '1', '0', '0'},
+                              {'1', '0', '1', '1', '1'},
+                              {'1', '1', '1', '1', '1'},
+                              {'1', '0', '0', '1', '0'}}) == 6_i);
+    expect(maximal_rectangle({{'0'}}) == 0_i);
+  };
+
+  return EXIT_SUCCESS;
+}
