50330b0774e0f08e0687ec82ee4c6959c62ea463
Maxim Eryomenko <moeryomenko@gmail.com>
2023-01-27 22:37:22 +0300

    perf(c++): 85. Maximal Rectangle
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- solutions/largest_rectangle.cpp
+++ solutions/largest_rectangle.cpp
@@ -7,40 +7,36 @@
 #include <utility>
 #include <vector>
 
-auto largest_rectangle(std::vector<int> &&heights) -> int {
+auto largest_rectangle(std::vector<int> heights) -> int {
   std::vector<std::pair<int, int>> stack;
   heights.push_back(0);
-  int max_area = 0;
-  for (int i = 0; auto h : heights) {
-    int start = i;
-    while (!stack.empty() && stack.back().second > h) {
-      auto [index, height] = stack.back();
-      stack.pop_back();
+  return std::accumulate(heights.cbegin(), heights.cend(), 0,
+                         [&stack, i = 0](auto max, auto h) mutable {
+                           int start = i;
+                           while (!stack.empty() && stack.back().second > h) {
+                             auto [index, height] = stack.back();
+                             stack.pop_back();
 
-      max_area = std::max(max_area, height * (i - index));
-      start = index;
-    }
-    stack.push_back({start, h});
-    ++i;
-  }
-  return max_area;
+                             max = std::max(max, height * (i - index));
+                             start = index;
+                           }
+                           stack.push_back({start, h});
+                           ++i;
+
+                           return max;
+                         });
 }
 
 auto maximal_rectangle(const std::vector<std::vector<char>> &matrix) -> int {
-  std::vector<std::vector<int>> dp(matrix.size());
-  std::inclusive_scan(
-      matrix.cbegin(), matrix.cend(), dp.begin(),
-      [](std::vector<int> prev_row, std::vector<char> row) {
-        std::transform(prev_row.cbegin(), prev_row.cend(), prev_row.begin(),
-                       [v = row.begin()](int n) mutable {
-                         return *v++ == '1' ? n + 1 : 0;
-                       });
-        return prev_row;
-      },
-      std::vector<int>(matrix.front().size(), 0));
-  return std::accumulate(dp.cbegin(), dp.cend(), 0, [](int max, auto heights) {
-    return std::max(max, largest_rectangle(std::move(heights)));
-  });
+  return std::accumulate(matrix.cbegin(), matrix.cend(), 0,
+                         [row = std::vector(matrix.front().size(), 0)](
+                             auto max, auto chars) mutable {
+                           std::transform(row.cbegin(), row.cend(), row.begin(),
+                                          [v = chars.begin()](int n) mutable {
+                                            return *v++ == '1' ? n + 1 : 0;
+                                          });
+                           return std::max(max, largest_rectangle(row));
+                         });
 }
 
 auto main() -> int {
